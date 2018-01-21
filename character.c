0d520d53f8a170ac4ec1f50f09ae8b536e56f35a
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-01-21 11:32:35 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	int g=0,c=0,i;
+	gets(a);
+	for(i=0;a[i]!=NULL;i++)
+	{
+		if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
+		b=++c;
+	}
+	printf("%d",g);
+	return 0;
+}

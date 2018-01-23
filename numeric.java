12233d5cf18018f14d199704161f71851bc14630
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-01-23 16:29:23 +0530

    Update numeric.java

--- numeric.java
+++ numeric.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class CheckDigits 
 {
-	public static void main(String[] args)
+public static void main(String[] args)
   {
-  
-		Scanner Kb=new Scanner(System.in);
-		System.out.println("How many strings  to check");
-		int t=Kb.nextInt();
-		while(t-->0){
-			System.out.println("Enter the string!");
-			String s=Kb.next();
-			int digits=0;
-			for(int i=0;i<s.length();i++)
+Scanner Kb=new Scanner(System.in);
+System.out.println("How many strings  to check");
+int t=Kb.nextInt();
+while(t-->0)
+{
+System.out.println("Enter the string!");
+String s=Kb.next();
+int digits=0;
+for(int i=0;i<s.length();i++)
       {
-				if(Character.isDigit(s.charAt(i)))
+if(Character.isDigit(s.charAt(i)))
         {
-					digits++;
+	digits++;
 				}
 			}
-			System.out.println(digits);
+	System.out.println(digits);
 		}
-		System.out.println();
+	System.out.println();
 	}
 
 }

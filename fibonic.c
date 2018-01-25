208d4112a80b97432ba85318f42f2b935df8f1a9
Ganeshamoorthy <35378548+Ganeshamoorthy@users.noreply.github.com>
2018-01-25 16:32:14 +0530

    Update fibonic.c

--- fibonic.c
+++ fibonic.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
+
 void main()
 {
-int g,b,n,i,m;
-printf("enter the number:");
-scanf("%d",&n);
-g=0;
-b=1;
-m=0;
-for(i=0;i<n;i++)
-{
-printf("%d\n",m);
-g=b;
-b=m;
-m=g+b;
+	int g,i,n,m,b;
+	printf("enter the number");
+	scanf("%d",&n);
+	g=0;
+	b=1;
+	m=0;
+	for(i=0;i<n;i++)
+	{
+             printf("%d\n",m);
+             g=b;
+             b=m;
+             m=g+b;
+             
+             
+	}
 }
-}
+
